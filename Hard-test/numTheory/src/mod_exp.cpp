27d7e851fe5ada373f14c547c2086a5ce23b5d6d
Siddd-hrr <siddheshakole@gmail.com>
2026-02-16 14:02:47 +0000

    27th-commit

--- Hard-test/numTheory/src/mod_exp.cpp
+++ Hard-test/numTheory/src/mod_exp.cpp
@@ -1,19 +1,19 @@
 #include <Rcpp.h>
+#include <cmath>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
 long long mod_exp(double base_in, double exp_in, double mod_in) {
   try {
-    // parse to integer 
     long long base = (long long)std::llround(base_in);
     long long exp  = (long long)std::llround(exp_in);
     long long mod  = (long long)std::llround(mod_in);
 
     if (mod <= 0) {
-      stop("mod_exp() requires modulus > 0.");
+      Rcpp::stop("mod_exp() requires modulus > 0.");
     }
     if (exp < 0) {
-      stop("mod_exp() does not support negative exponents.");
+      Rcpp::stop("mod_exp() does not support negative exponents.");
     }
 
     long long result = 1;
@@ -30,9 +30,9 @@ long long mod_exp(double base_in, double exp_in, double mod_in) {
     return result;
 
   } catch (std::exception &ex) {
-    stop("Error in mod_exp(): %s", ex.what());
+    Rcpp::stop("Error in mod_exp(): %s", ex.what());
   } catch (...) {
-    stop("Unknown error in mod_exp().");
+    Rcpp::stop("Unknown error in mod_exp().");
   }
 }
 
